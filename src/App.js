59864cb12e893bc6ef6eebcd1a2596cb15e0f87f
Amrit Thapa <thapa.amet27@gmail.com>
2021-08-18 11:25:11 +0545

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import MenuBar from "./components/MenuBar";
 import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <Router>
       <MenuBar />
-      <Home />
-    </div>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signin" exact component={Login} />
+        <Route path="/signup" exact component={Register} />
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+
 const MenuBar = () => {
   return (
     <div>
       <ul className="flex justify-around items-center p-4 m-auto w-3/4">
-        <li>Sign Up</li>
-        <li>Sign In</li>
+        <li>
+          <Link to="/signup">Sign Up</Link>
+        </li>
+        <li>
+          <Link to="/signin">Sign In</Link>
+        </li>
       </ul>
     </div>
   );
