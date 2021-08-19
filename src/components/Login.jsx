2c6656239f2b37ea836d1fbb57b8fc81efce3376
Amrit Thapa <thapa.amet27@gmail.com>
2021-08-19 22:06:52 +0545

    remember me portion
    and other functionalities on formik field values

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -20,39 +20,59 @@ const Login = () => {
         console.log(values);
       }}
     >
-      <div className="h-full flex flex-col justify-center items-center relative">
-        <div className="text-left font-bold text-2xl">Account Login</div>
-        <Form>
-          <TextField
-            label="E-mail"
-            name="email"
-            type="email"
-            placeholder="E-mail"
-          />
-          <TextField
-            label="Password"
-            name="password"
-            type="password"
-            placeholder="Password"
-          />
-          <input
-            className="mt-8 mr-3"
-            name="remember"
-            type="checkbox"
-            value="1"
-          />
-          <label>Remember me</label>
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold mt-5 py-2 px-4 rounded w-full"
-          >
-            Sign in
-          </button>
-        </Form>
-        <button className="text-green-500 hover:text-green-600 font-semibold text-lg transform hover:scale-125 transition duration-200 ease-in rounded absolute bottom-0">
-          <Link to="/signup">Register</Link>
-        </button>
-      </div>
+      {(formik) => {
+        return (
+          <div className="h-full flex flex-col justify-center items-center relative">
+            <div className="text-left font-bold text-2xl">Account Login</div>
+            <Form>
+              <TextField
+                label="E-mail"
+                name="email"
+                type="email"
+                placeholder="E-mail"
+              />
+              <TextField
+                label="Password"
+                name="password"
+                type="password"
+                placeholder="Password"
+              />
+              <input
+                onChange={(e) => {
+                  formik.setFieldValue("remember", e.target.checked);
+                }}
+                className="mt-8 mr-3"
+                name="remember"
+                type="checkbox"
+                checked={formik.values.remember}
+              />
+
+              <label>Remember me</label>
+              <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-600 text-white font-semibold mt-5 py-2 px-4 rounded w-full"
+              >
+                Sign in
+              </button>
+              <button
+                type="button"
+                onClick={() => {
+                  console.log(formik.values);
+                  if (formik.values.email !== "amrit.thapa@gmail.com") {
+                    // formik.setFieldTouched("email", true);
+                    formik.setFieldError("email", "You are not authorised");
+                  }
+                }}
+              >
+                Validate
+              </button>
+            </Form>
+            <button className="text-green-500 hover:text-green-600 font-semibold text-lg transform hover:scale-125 transition duration-200 ease-in rounded absolute bottom-0">
+              <Link to="/signup">Register</Link>
+            </button>
+          </div>
+        );
+      }}
     </Formik>
   );
 };
--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,17 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const MenuBar = () => {
   return (
     <div className="bg-primary font-bold text-gray-300 rounded-l-lg">
       <ul className="flex flex-col items-center justify-center h-full m-auto">
         <li className="p-4">
-          <Link to="/">Home</Link>
+          <NavLink exact activeClassName="text-white" to="/">
+            Home
+          </NavLink>
         </li>
         <li className="p-4">
-          <Link to="/signup">Sign Up</Link>
+          <NavLink exact activeClassName="text-white" to="/signup">
+            Sign Up
+          </NavLink>
         </li>
         <li className="p-4">
-          <Link to="/signin">Sign In</Link>
+          <NavLink exact activeClassName="text-white" to="/signin">
+            Sign In
+          </NavLink>
         </li>
       </ul>
     </div>
