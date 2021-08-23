ff4b78fa32bcd27844e8b5662ff5bb3f8975d9bf
Amrit Thapa <thapa.amet27@gmail.com>
2021-08-23 22:22:40 +0545

    spp js
    removed unwanted components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import MenuBar from "./components/MenuBar";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
