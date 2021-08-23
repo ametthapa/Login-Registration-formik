96970ccc81fbd0059d0bab70378aad65ba9c7477
Amrit Thapa <thapa.amet27@gmail.com>
2021-08-23 14:49:38 +0545

    index js
    wrapped with router

--- src/App.js
+++ src/App.js
@@ -31,21 +31,19 @@ function App() {
   }, []);
   return (
     <div className=" px-10 py-5  lg:w-3/4 m-auto">
-      <Router>
-        {/* <MenuBar className="col-span-1" /> */}
-        <div className=" rounded-r-lg bg-gray-100 p-5 h-screen">
-          <Switch>
-            {loggedIn ? (
-              <Route path="/" exact component={Home} />
-            ) : (
-              <Fragment>
-                <Route path="/signin" exact component={Login} />
-                <Route path="/signup" exact component={Register} />
-              </Fragment>
-            )}
-          </Switch>
-        </div>
-      </Router>
+      {/* <MenuBar className="col-span-1" /> */}
+      <div className=" rounded-r-lg bg-gray-100 p-5 h-screen">
+        <Switch>
+          {loggedIn ? (
+            <Route path="/" exact component={Home} />
+          ) : (
+            <Fragment>
+              <Route path="/signin" exact component={Login} />
+              <Route path="/signup" exact component={Register} />
+            </Fragment>
+          )}
+        </Switch>
+      </div>
     </div>
   );
 }
